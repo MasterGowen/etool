e86515085a038a636db300c9a982cba79f80eff1
MasterGowen <mastergowen@gmail.com>
2018-09-06 16:28:12 +0500

    login required

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth.decorators import login_required
 from .views import index, dashboard, project, PersonUpdate
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', login_required(index), name="index"),
     path('dashboard/', dashboard, name="dashboard"),
     path('person/', PersonUpdate.as_view(), name="person_update"),
     path('projects/<int:pk>/', project),
