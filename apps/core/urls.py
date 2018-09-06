2f49709c769788c51b8a56907eff83d7fece6579
MasterGowen <mastergowen@gmail.com>
2018-09-06 16:28:58 +0500

    login required#1

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,7 +4,7 @@ from .views import index, dashboard, project, PersonUpdate
 
 urlpatterns = [
     path('', login_required(index), name="index"),
-    path('dashboard/', dashboard, name="dashboard"),
-    path('person/', PersonUpdate.as_view(), name="person_update"),
+    path('dashboard/', login_required(dashboard), name="dashboard"),
+    path('person/', login_required(PersonUpdate.as_view()), name="person_update"),
     path('projects/<int:pk>/', project),
 ]
